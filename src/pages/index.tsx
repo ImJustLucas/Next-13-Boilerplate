72891bce536fd07b0137eebf3c2c3c82293f713b
ImJustLucas <lucas.bellier@icloud.com>
2023-10-03 16:46:10 +0200

    📦 add styled component to boilerplate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,32 @@
+import styled from "styled-components";
+
 export default function Home() {
   return (
-    <>
-      <h1>Just a boilerplate for next</h1>
-      <p>
+    <Container>
+      <Title>Just a boilerplate for next</Title>
+      <Subtitle>
         Magnificent Next.js boilerplate with TypeScript, ESLint, Prettier and
         everything else you need to bootstrap your Next.js project.
-      </p>
-    </>
+      </Subtitle>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.h1`
+  font-size: 50px;
+  text-align: center;
+`;
+// color: ${({ theme }) => theme.colors.primary};
+const Subtitle = styled.p`
+  font-size: 24px;
+  max-width: 500px;
+  text-align: center;
+  margin-top: 32px;
+`;
